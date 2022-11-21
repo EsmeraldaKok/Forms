a65b2c91b4ebe008bcb04e5ede6391c2dc3bc3dd
EsmeraldaKok <EKok@exigent-group.com>
2022-11-21 22:55:27 +0200

    some minor changes

--- Forms/forms.js
+++ Forms/forms.js
@@ -1,18 +1,18 @@
-var inputLeft = document.getElementById("input-left");
-var inputRight = document.getElementById("input-right");
+let inputLeft = document.getElementById("input-left");
+let inputRight = document.getElementById("input-right");
 
-var thumbLeft = document.querySelector(".slider > .thumb.left");
-var thumbRight = document.querySelector(".slider > .thumb.right");
-var range = document.querySelector(".slider > .range");
+let thumbLeft = document.querySelector(".slider > .thumb.left");
+let thumbRight = document.querySelector(".slider > .thumb.right");
+let range = document.querySelector(".slider > .range");
 
 function setLeftValue() {
-	var _this = inputLeft,
+	let _this = inputLeft,
 		min = parseInt(_this.min),
 		max = parseInt(_this.max);
 
 	_this.value = Math.min(parseInt(_this.value), parseInt(inputRight.value) - 1);
 
-	var percent = ((_this.value - min) / (max - min)) * 100;
+	let percent = ((_this.value - min) / (max - min)) * 100;
 
 	thumbLeft.style.left = percent + "%";
 	range.style.left = percent + "%";
@@ -20,13 +20,13 @@ function setLeftValue() {
 setLeftValue();
 
 function setRightValue() {
-	var _this = inputRight,
+	let _this = inputRight,
 		min = parseInt(_this.min),
 		max = parseInt(_this.max);
 
 	_this.value = Math.max(parseInt(_this.value), parseInt(inputLeft.value) + 1);
 
-	var percent = ((_this.value - min) / (max - min)) * 100;
+	let percent = ((_this.value - min) / (max - min)) * 100;
 
 	thumbRight.style.right = (100 - percent) + "%";
 	range.style.right = (100 - percent) + "%";
